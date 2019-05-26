ad9b727ed4c545d6a5af85c7a48f5fd7102f4409
Shemozzle <41769935+Shemozzle@users.noreply.github.com>
2019-05-27 09:40:22 +1000

    Added 10 questions
    10 questions about 310/510 are added.

--- js/questions.js
+++ js/questions.js
@@ -1,91 +1,95 @@
 
 let $questions = [{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-}, 
+                "question 1" : "Planning is the intelligent estimate of resources required to perform a predefined project successfully at a future date within a defined environment.",
+                "a": "T",
+                "b": "F",
+                "c": "NA",
+                "d": "NA",
+                "answer": "T"
+         }, 
+         
+         
+         {
+                "question 2": "What attributes DOES NOT make planning for software project unique?" : "",
+                "a": "Functionality is the primary output",
+                "b": "Productivity and quality are dependent on humans",
+                "c": "Output is physical.",
+                "d": "Software engineering tools have limited predictability",
+                "answer": "c"
+         }, 
+         
+         {
+                "question 3" : "What is NOT a key feature of the project management plan?",
+                "a": "Estimate: predict status of project",
+                "b": "Resources: machines, personnel, materials, etc.",
+                "c": "Dates: milestones, end date, etc.",
+                "d": "Best practices: things learnt from other projects",
+                "answer": "d"
+         }, 
+         
 {
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
+        "question 4" : "What is not a best practice in software project planning",
+        "a": "Creating a work register",
+        "b": "Perform variance analysis to uncover the difference between actual and planned behaviour",
+        "c": "Norms for planning where knowledge repository is important",
+        "d": "Following a process driven approach",
+        "answer": "a"
 },  
 {
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
+        "question 5" : "What factors can result in scope creep",
+        "a": "Changing user requirements ",
+        "b": "Poorly defined scope statements ",
+        "c": "Business analyst errors or underestimation ",
+        "d": "All of the above",
+        "answer": "d"
 },  
 {
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
+        "question 5" : "How can project effort increase?",
+        "a": "scope creep",
+        "b": "low productivity",
+        "c": "poor morale",
+        "d": "all of the above",
+        "answer": "d"
 },  
 {
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
+        "question 6" : "The 6 classes of software reporting metrics are quality, schedule, productivity, cost, change and milestones",
+        "a": "T",
+        "b": "F",
+        "c": "NA",
+        "d": "NA",
+        "answer": "F"
 },  
 {
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
+        "question 7" : "What does the work break down structure consist of?",
+        "a": "Start milestone",
+        "b": "Internal mileston",
+        "c": "End milestone",
+        "d": "All of the above",
+        "answer": "d"
 },  
 {
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
+        "question 8" : "What is not a benefit of prince 2",
+        "a": "Allows early warning and detection of problems.",
+        "b": "Consistent set of benchmarks, processes, and procedures.",
+        "c": "Quick iteration springs of the project",
+        "d": "Official certification and qualification scheme for professionals.",
+        "answer": "c"
 },  
 {
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
+        "question 9" : "Agile Involves Multidisciplinary Teams Interacting and Working Toward a Common Long Term Goal.",
+        "a": "T",
+        "b": "F",
+        "c": "NA",
+        "d": "NA",
+        "answer": "F"
 },  
 {
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
+        "question 10" : "A common pitfall for software project initiation is a poor kick-off meeting",
+        "a": "T",
+        "b": "F",
+        "c": "NA",
+        "d": "nA",
+        "answer": "F"
 },  
 {
         "question" : "",
