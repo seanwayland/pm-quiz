f0ff2be696715c57c65ca278b9d3d86917b6de92
Shemozzle <41769935+Shemozzle@users.noreply.github.com>
2019-05-27 10:07:27 +1000

    Reformatted the questions
    Removed some T/F questions and replaced them. Also adjusted the formatting of the question set.

--- js/questions.js
+++ js/questions.js
@@ -1,34 +1,30 @@
 
 let $questions = [{
-                "question 1" : "Planning is the intelligent estimate of resources required to perform a predefined project successfully at a future date within a defined environment.",
-                "a": "T",
-                "b": "F",
-                "c": "NA",
-                "d": "NA",
-                "answer": "T"
-         }, 
-         
-         
-         {
-                "question 2": "What attributes DOES NOT make planning for software project unique?" : "",
-                "a": "Functionality is the primary output",
-                "b": "Productivity and quality are dependent on humans",
-                "c": "Output is physical.",
-                "d": "Software engineering tools have limited predictability",
-                "answer": "c"
-         }, 
-         
-         {
-                "question 3" : "What is NOT a key feature of the project management plan?",
-                "a": "Estimate: predict status of project",
-                "b": "Resources: machines, personnel, materials, etc.",
-                "c": "Dates: milestones, end date, etc.",
-                "d": "Best practices: things learnt from other projects",
-                "answer": "d"
-         }, 
-         
+        "question" : "Planning is the intelligent estimate of resources required to perform a predefined project successfully at a future date within a defined environment.",
+        "a": "T",
+        "b": "F",
+        "c": "NA",
+        "d": "NA",
+        "answer": "a"
+},       
 {
-        "question 4" : "What is not a best practice in software project planning",
+        "question": "What attributes DOES NOT make planning for software project unique?",
+        "a": "Functionality is the primary output",
+        "b": "Productivity and quality are dependent on humans",
+        "c": "Output is physical.",
+        "d": "Software engineering tools have limited predictability",
+        "answer": "c"
+},         
+{
+        "question" : "What is NOT a key feature of the project management plan?",
+        "a": "Estimate: predict status of project",
+        "b": "Resources: machines, personnel, materials, etc.",
+        "c": "Dates: milestones, end date, etc.",
+        "d": "Best practices: things learnt from other projects",
+        "answer": "d"
+},         
+{
+        "question" : "What is not a best practice in software project planning",
         "a": "Creating a work register",
         "b": "Perform variance analysis to uncover the difference between actual and planned behaviour",
         "c": "Norms for planning where knowledge repository is important",
@@ -36,7 +32,7 @@ let $questions = [{
         "answer": "a"
 },  
 {
-        "question 5" : "What factors can result in scope creep",
+        "question" : "What factors can result in scope creep",
         "a": "Changing user requirements ",
         "b": "Poorly defined scope statements ",
         "c": "Business analyst errors or underestimation ",
@@ -44,7 +40,7 @@ let $questions = [{
         "answer": "d"
 },  
 {
-        "question 5" : "How can project effort increase?",
+        "question" : "How can project effort increase?",
         "a": "scope creep",
         "b": "low productivity",
         "c": "poor morale",
@@ -52,15 +48,15 @@ let $questions = [{
         "answer": "d"
 },  
 {
-        "question 6" : "The 6 classes of software reporting metrics are quality, schedule, productivity, cost, change and milestones",
-        "a": "T",
-        "b": "F",
-        "c": "NA",
-        "d": "NA",
-        "answer": "F"
+        "question" : "What is not one of the five questions of service definition",
+        "a": "Service description",
+        "b": "Service delivery",
+        "c": "Service provision",
+        "d": "Service support",
+        "answer": "c"
 },  
 {
-        "question 7" : "What does the work break down structure consist of?",
+        "question" : "What does the work break down structure consist of?",
         "a": "Start milestone",
         "b": "Internal mileston",
         "c": "End milestone",
@@ -68,7 +64,7 @@ let $questions = [{
         "answer": "d"
 },  
 {
-        "question 8" : "What is not a benefit of prince 2",
+        "question" : "What is not a benefit of prince 2",
         "a": "Allows early warning and detection of problems.",
         "b": "Consistent set of benchmarks, processes, and procedures.",
         "c": "Quick iteration springs of the project",
@@ -76,756 +72,19 @@ let $questions = [{
         "answer": "c"
 },  
 {
-        "question 9" : "Agile Involves Multidisciplinary Teams Interacting and Working Toward a Common Long Term Goal.",
+        "question" : "What is NOT a characteristic of Agile development?",
+        "a": "Daily feedback and self directed team",
+        "b": "Hard touch",
+        "c": "Open information",
+        "d": "Guiding information",
+        "answer": "b"
+},  
+{
+        "question" : "Poor kick-off meetings is a common pitfall of software project initiation?",
         "a": "T",
         "b": "F",
         "c": "NA",
         "d": "NA",
-        "answer": "F"
+        "answer": "b"
 },  
-{
-        "question 10" : "A common pitfall for software project initiation is a poor kick-off meeting",
-        "a": "T",
-        "b": "F",
-        "c": "NA",
-        "d": "nA",
-        "answer": "F"
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-},  
-{
-        "question" : "",
-        "a": "",
-        "b": "",
-        "c": "",
-        "d": "",
-        "answer": ""
-}, 
-
 ];
