69b73dd5432a5fa933a9eebf4105ab5f32a692ba
seanwayland <seanwayland@gmail.com>
2019-05-27 20:49:50 -0400

    added 5 questions

--- js/questions.js
+++ js/questions.js
@@ -1,146 +1,189 @@
-
 let $questions = [{
-        "question" : "Planning is the intelligent estimate of resources required to perform a predefined project successfully at a future date within a defined environment.",
-        "a": "True",
-        "b": "False",
-        "c": "NA",
-        "d": "NA",
-        "answer": "a"
-},       
-{
+    "question": "Planning is the intelligent estimate of resources required to perform a predefined project successfully at a future date within a defined environment.",
+    "a": "True",
+    "b": "False",
+    "c": "NA",
+    "d": "NA",
+    "answer": "a"
+},
+    {
         "question": "What attributes DOES NOT make planning for software project unique?",
         "a": "Functionality is the primary output",
         "b": "Productivity and quality are dependent on humans",
         "c": "Output is physical.",
         "d": "Software engineering tools have limited predictability",
         "answer": "c"
-},         
-{
-        "question" : "What is NOT a key feature of the project management plan?",
+    },
+    {
+        "question": "What is NOT a key feature of the project management plan?",
         "a": "Estimate: predict status of project",
         "b": "Resources: machines, personnel, materials, etc.",
         "c": "Dates: milestones, end date, etc.",
         "d": "Best practices: things learnt from other projects",
         "answer": "d"
-},         
-{
-        "question" : "What is not a best practice in software project planning",
+    },
+    {
+        "question": "What is not a best practice in software project planning",
         "a": "Creating a work register",
         "b": "Perform variance analysis to uncover the difference between actual and planned behaviour",
         "c": "Norms for planning where knowledge repository is important",
         "d": "Following a process driven approach",
         "answer": "a"
-},  
-{
-        "question" : "What factors can result in scope creep",
+    },
+    {
+        "question": "What factors can result in scope creep",
         "a": "Changing user requirements ",
         "b": "Poorly defined scope statements ",
         "c": "Business analyst errors or underestimation ",
         "d": "All of the above",
         "answer": "d"
-},  
-{
-        "question" : "How can project effort increase?",
+    },
+    {
+        "question": "How can project effort increase?",
         "a": "scope creep",
         "b": "low productivity",
         "c": "poor morale",
         "d": "all of the above",
         "answer": "d"
-},  
-{
-        "question" : "What is not one of the five questions of service definition",
+    },
+    {
+        "question": "What is not one of the five questions of service definition",
         "a": "Service description",
         "b": "Service delivery",
         "c": "Service provision",
         "d": "Service support",
         "answer": "c"
-},  
-{
-        "question" : "What does the work break down structure consist of?",
+    },
+    {
+        "question": "What does the work break down structure consist of?",
         "a": "Start milestone",
         "b": "Internal mileston",
         "c": "End milestone",
         "d": "All of the above",
         "answer": "d"
-},  
-{
-        "question" : "What is not a benefit of prince 2",
+    },
+    {
+        "question": "What is not a benefit of prince 2",
         "a": "Allows early warning and detection of problems.",
         "b": "Consistent set of benchmarks, processes, and procedures.",
         "c": "Quick iteration sprints of the project",
         "d": "Official certification and qualification scheme for professionals.",
         "answer": "c"
-},  
-{
-        "question" : "What is NOT a characteristic of Agile development?",
+    },
+    {
+        "question": "What is NOT a characteristic of Agile development?",
         "a": "Daily feedback and self directed team",
         "b": "Hard touch",
         "c": "Open information",
         "d": "Guiding information",
         "answer": "b"
-},  
-{
-        "question" : "Poor kick-off meetings is a common pitfall of software project initiation?",
+    },
+    {
+        "question": "Poor kick-off meetings is a common pitfall of software project initiation?",
         "a": "True",
         "b": "False",
         "c": "NA",
         "d": "NA",
         "answer": "b"
-},  
-{
-        "question" : "Which of the following is not considered a criteria for success?",
+    },
+    {
+        "question": "Which of the following is not considered a criteria for success?",
         "a": "The project was on cost",
         "b": "The project was on time",
         "c": "The software specification and the user requirements were met",
         "d": "Minimal change requests from the client",
         "answer": "c"
-},
-{
-        "question" : "Project managers have to assess the risks that may affect a project.",
+    },
+    {
+        "question": "Project managers have to assess the risks that may affect a project.",
         "a": "True",
         "b": "False",
         "c": "None",
         "d": "Both",
         "answer": "b"
-},  
-{
-        "question" : "Which of the following is not considered as a risk in project management?",
+    },
+    {
+        "question": "Which of the following is not considered as a risk in project management?",
         "a": "Specification delays",
         "b": "Product competition",
         "c": "Testing",
         "d": "Staff turnover",
         "answer": "c"
-},  
-{
-        "question" : "The process each manager follows during the life of a project is known as:",
+    },
+    {
+        "question": "The process each manager follows during the life of a project is known as:",
         "a": "Project Management",
         "b": "Manager life cycle",
         "c": "Project Management Life Cycle",
         "d": "All of the above",
         "answer": "c"
-},  
-{
-        "question" : "A general statement of objectives is the major cause of failed software efforts.",
+    },
+    {
+        "question": "A general statement of objectives is the major cause of failed software efforts.",
         "a": "True",
         "b": "False",
         "c": "None",
         "d": "Both",
         "answer": "a"
-},  
-{
-        "question" : "Requirements traceability is one of the most important part requirement management. It may also be referred to as the heart of requirement management.",
+    },
+    {
+        "question": "Requirements traceability is one of the most important part requirement management. It may also be referred to as the heart of requirement management.",
         "a": "True",
         "b": "False",
         "c": "None",
         "d": "Both",
         "answer": "a"
-},  
-{
-        "question" : "According to a statistical report: 'over 30% of all software projects are cancelled before completion and over 70% of the remainder fail to deliver expected features'. What must be the reason for such a situation?",
+    },
+    {
+        "question": "According to a statistical report: 'over 30% of all software projects are cancelled before completion and over 70% of the remainder fail to deliver expected features'. What must be the reason for such a situation?",
         "a": "Poor change management",
         "b": "Poor requirements management",
         "c": "Poor quality control",
         "d": "All of the above",
         "answer": "b"
-},  
+    },
+    {
+        "question": "The acronym COCOMO stands for ",
+        "a": "Create. Change, Measure",
+        "b": "Control, Create, Manage",
+        "c": "Capitalisation Creation Model",
+        "d": "Constructive Cost Model",
+        "answer": "d"
+    },
+    {
+        "question": "Fagan inspections refers to ",
+        "a": "Measuring the hours taken to complete a milestone",
+        "b": "Formal review of the code",
+        "c": "The project manager's regular visits to software project manager to discuss progress",
+        "d": "Fagan is a program developed by Stanford university to study version changes",
+        "answer": "b"
+    },
+    {
+        "question": "In software project management CRM refers to ",
+        "a": "Copyright protection introduced into software",
+        "b": "Code Review Milestone",
+        "c": "Customer relationship management",
+        "d": "Code repository manager",
+        "answer": "c"
+    }
+    ,
+    {
+        "question": "Instead of Gantt charts, Agile projects use a ___  ",
+        "a": "Backlog",
+        "b": "Spreadsheet",
+        "c": "Carp chart ( code review process chart ) ",
+        "d": "Post-it note",
+        "answer": "a"
+    }
+    ,
+    {
+        "question": "Which of these questions is not usually asked at an agile project Daily Stand up meeting?",
+        "a": "Are Any Issues Blocking Progress?",
+        "b": "What Tasks were completed?",
+        "c": "What Was Accomplished?",
+        "d": "How long do you estimate it will take to finish this task",
+        "answer": "d"
+    }
+
+
 ];
